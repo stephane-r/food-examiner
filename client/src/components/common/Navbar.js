79cc4056e7e51bf42ce5be898ff31997973d6ceb
williamang2736 <williamang2736@gmail.com>
2019-02-22 11:13:08 +0800

    Updated navbar and spinner utilities

--- client/src/components/common/Navbar.js
+++ client/src/components/common/Navbar.js
@@ -4,18 +4,18 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-light bg-light justify-content-between">
       <a className="navbar-brand">Navbar</a>
-      <ul className="navbar-nav flex-row justify-content-between" style={{ flexBasis: '20%' }}>
-        <li className="nav-item active"  >
+      <ul className="navbar-nav flex-row justify-content-end">
+        <li className="nav-item active" style={{ paddingLeft: '10px', paddingRight: '10px'}} >
           <a className="nav-link" href="#">
             Food Recognition <span className="sr-only">(current)</span>
           </a>
         </li>
-        <li className="nav-item">
+        <li className="nav-item" style={{ paddingLeft: '10px', paddingRight: '10px'}} >
           <a className="nav-link" href="#">
             Features
           </a>
         </li>
-        <li className="nav-item">
+        <li className="nav-item" style={{ paddingLeft: '10px', paddingRight: '10px'}} >
           <a className="nav-link" href="#">
             Pricing
           </a>
