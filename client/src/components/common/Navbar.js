25875ea46ef7856acdbe136a573819b2eb644d83
williamang2736 <williamang2736@gmail.com>
2019-02-22 20:47:39 +0800

    Made layout responsive

--- client/src/components/common/Navbar.js
+++ client/src/components/common/Navbar.js
@@ -3,23 +3,9 @@ import React from "react";
 export default function Navbar() {
   return (
     <nav className="navbar navbar-light bg-light justify-content-between">
-      <a className="navbar-brand">Navbar</a>
+      <a className="navbar-brand">Food Recognition</a>
       <ul className="navbar-nav flex-row justify-content-end">
-        <li className="nav-item active" style={{ paddingLeft: '10px', paddingRight: '10px'}} >
-          <a className="nav-link" href="#">
-            Food Recognition <span className="sr-only">(current)</span>
-          </a>
-        </li>
-        <li className="nav-item" style={{ paddingLeft: '10px', paddingRight: '10px'}} >
-          <a className="nav-link" href="#">
-            Features
-          </a>
-        </li>
-        <li className="nav-item" style={{ paddingLeft: '10px', paddingRight: '10px'}} >
-          <a className="nav-link" href="#">
-            Pricing
-          </a>
-        </li>
+        
       </ul>
     </nav>
   );
