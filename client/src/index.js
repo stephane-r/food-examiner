88890bf0d5faf80d3c217efc3d80eb3ecb115d46
williamang2736 <williamang2736@gmail.com>
2019-02-27 23:24:13 +0800

    Added redux dev tools

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+
+import store from './store';
 import "./bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.min.css";
 import App from "./App";
-
-import * as serviceWorker from "./serviceWorker";
-
-import { Provider } from "react-redux";
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
-
 import "./utilities.css";
-import rootReducer from "./reducers";
-
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk)
-);
 
 ReactDOM.render(
   <Provider store={store}>
