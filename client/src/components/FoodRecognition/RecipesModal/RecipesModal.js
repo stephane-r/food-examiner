a5cc1741015f187fde5e2518811c33f5097112bd
williamang2736 <williamang2736@gmail.com>
2019-02-28 17:00:54 +0800

    Restricted height of recipes modal

--- client/src/components/FoodRecognition/RecipesModal/RecipesModal.js
+++ client/src/components/FoodRecognition/RecipesModal/RecipesModal.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 import RecipeBox from "./RecipeBox";
-import { fetchRecipes, toggleRecipeModal } from '../../../actions/recipesActions';
+import {
+  fetchRecipes,
+  toggleRecipeModal
+} from "../../../actions/recipesActions";
 import "./index.css";
 
 class RecipesModal extends React.Component {
-
   render() {
     return (
       <div>
-        <Button color="danger" onClick={this.props.toggleModal}>
-          View Recipes
-        </Button>
         <Modal
           size={"lg"}
           isOpen={this.props.modalIsOpen}
@@ -21,11 +20,13 @@ class RecipesModal extends React.Component {
           className={this.props.className}
         >
           <ModalHeader toggle={this.props.toggleModal}>Recipes</ModalHeader>
-          <ModalBody>
-            <div className="recipes">
-              {this._renderRecipes()}
-            </div>
-          </ModalBody>
+
+          <div
+            className="recipes"
+            style={{ height: "400px", maxHeight: "400px", overflowY: "scroll", padding: '10px' }}
+          >
+            {this._renderRecipes()}
+          </div>
           <ModalFooter>
             <div style={{ marginRight: "auto" }}>Recipes from recipe puppy</div>
           </ModalFooter>
@@ -36,13 +37,16 @@ class RecipesModal extends React.Component {
 
   _renderRecipes = () => {
     return this.props.recipes.map((r, i) => {
-      return <RecipeBox recipe={r} key={i}/>
+      return <RecipeBox recipe={r} key={i} />;
     });
   };
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return { ...state.recipes };
 };
 
-export default connect(mapStateToProps, { toggleModal: toggleRecipeModal, fetchRecipes })(RecipesModal);
+export default connect(
+  mapStateToProps,
+  { toggleModal: toggleRecipeModal, fetchRecipes }
+)(RecipesModal);
